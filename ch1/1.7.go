38d4318c65fb0f67a086c4a438bd13c96eb29ec5
bee-keeper <ricbottomley@gmail.com>
2018-08-28 19:01:09 +0100

    ch1

---
+++ ch1/1.7.go
@@ -0,0 +1,41 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 16.
+//!+
+
+// Fetch prints the content found at each specified URL.
+/*
+io.copy(dst, src) reads from src and copies to dst
+use it instead of ioutil.ReadAll to copy the response body to
+os.stdout without requiring a buffer large enough to hold the entire
+stream. Check the error result of io.copy
+*/
+
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		written, err := io.Copy(os.Stdout, resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+		fmt.Printf("%d bytes copied", written)
+	}
+}
+
+//!-
